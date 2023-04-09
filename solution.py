3697b4fb54b9460efd7ec717350b7738f1147529
Valeratal <test4you@yandex.ru>
2023-04-09 21:59:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+
+from scipy.stats import ks_2samp
 
 chat_id = 252926140 # your chat ID, don't change the variable name
 
 def solution(x: np.array, y: np.array) -> bool:
-  alpha = 0.09
-  t_stat, p_val = ttest_ind(x, y)
-  return p_val < alpha
+  _, p_value = ks_2samp(x, y)
+  return p_value <= 0.09
