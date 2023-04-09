5e80e3064e55cdb01448ce1118f43e85b64b0223
Valeratal <test4you@yandex.ru>
2023-04-09 20:59:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 252926140 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
